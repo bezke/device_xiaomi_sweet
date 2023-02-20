e68a4e08ed1931c0a3ce1e9707eb5239aeedc8e3
bezke <bezke1@gmail.com>
2023-02-20 07:27:39 +0100

    sweet: Let's Banana

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/spark_sweet.mk
+    $(LOCAL_DIR)/banana_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    spark_sweet-eng \
-    spark_sweet-userdebug \
-    spark_sweet-user
+    banana_sweet-eng \
+    banana_sweet-userdebug \
+    banana_sweet-user
--- banana_sweet.mk
+++ banana_sweet.mk
@@ -11,32 +11,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit common Spark-OS Stuff.
-$(call inherit-product, vendor/spark/config/common_full_phone.mk)
+# Inherit common Banana OS  Stuff.
+$(call inherit-product, vendor/banana/config/common.mk)
 
-# Face Unlock
-TARGET_FACE_UNLOCK_SUPPORTED := true
-
-# Blur
-TARGET_USES_BLUR := true
-
-# Bootanimation
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Pixel Stuff
-USE_PIXEL_CHARGER := true
-
-# Quick Tap
-TARGET_SUPPORTS_QUICK_TAP := true
-
-SPARK_BUILD_TYPE := InnOvaTioN
-WITH_GAPPS := true
-TARGET_NO_PREBUILT_CAMERA := true
-
-PRODUCT_NAME := spark_sweet
+PRODUCT_NAME := banana_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Banana Stuffs
+BANANA_MAINTAINER := bezke
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+WITH_GAPPS := true
+BANANA_EDITION := CoreGApps
